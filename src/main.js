dd80f23689c7ce26fac1267bf0ce095c5bd54dd8
Mfon Ukobo <mfon.ukobo@maxfront.com>
2022-05-03 16:09:40 +0100

    modified for package publishing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import MaxTable from './MaxTable.vue';
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export { MaxTable };
